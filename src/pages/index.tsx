bc66463f72efdd39d07b12692afc0726f52ba88f
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2024-02-23 15:02:04 +0100

    Change post font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
